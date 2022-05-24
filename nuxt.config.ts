023f451a91e2afed7491c36e1309649111e0c820
Digby Knight <digby@digby.net>
2022-05-24 22:17:43 +0100

    Minifying js and other things

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,21 @@ export default defineNuxtConfig({
   privateRuntimeConfig: {
     myPrivateToken: process.env.PRIVATE_TOKEN,
   },
+  build: {
+    html: {
+      minify: {
+        collapseBooleanAttributes: true,
+        decodeEntities: true,
+        minifyCSS: true,
+        minifyJS: true,
+        processConditionalComments: true,
+        removeEmptyAttributes: true,
+        removeRedundantAttributes: true,
+        trimCustomFragments: true,
+        useShortDoctype: true,
+      },
+    },
+  },
   head: {
     htmlAttrs: {
       lang: "en",
