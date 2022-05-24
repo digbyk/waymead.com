92ad2825cdcf4da77258c0eacf5eac10da43b307
Digby Knight <digby@digby.net>
2022-05-24 21:59:11 +0100

    Adding meta stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,14 @@ export default defineNuxtConfig({
     myPrivateToken: process.env.PRIVATE_TOKEN,
   },
   head: {
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "waymead.com" },
+    ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
