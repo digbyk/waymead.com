e40f52eb97bcbfce579fbe4fbb547148f2ad4ff6
Digby Knight <digby@digby.net>
2022-05-28 08:12:19 +0100

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,7 +26,7 @@ export default defineNuxtConfig({
       },
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/x-icon", href: "/icons/favicon.ico" },
       {
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Roboto:wght@100;300;400;700&display=swap",
