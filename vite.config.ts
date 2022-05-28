1beddd8bbb8e92582b7078fd9883069d05e23876
Digby Knight <digby@digby.net>
2022-05-28 10:39:51 +0100

    Removing Vite PWA. Waiting for Nuxt one

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,3 @@
-import { VitePWA } from "vite-plugin-pwa";
 export default {
-  plugins: [
-    VitePWA({
-      manifest: {
-        name: "Name of your app",
-        short_name: "Short name of your app",
-        description: "Description of your app",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [],
 };
