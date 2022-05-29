0b61ec8311194269e4bdca4ea9c40391516ff4f3
Digby Knight <digby@digby.net>
2022-05-29 07:29:13 +0100

    Fixing layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,7 +29,7 @@ definePageMeta({
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;700&family=Lato:wght@300;400;700&family=Roboto:wght@100;300;400;700&display=swap");
 html,
 body,
 #__nuxt {
