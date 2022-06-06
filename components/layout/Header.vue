da75e6d4c2f836f3592549f6fc5ae2af36c0718a
Digby Knight <digby@digby.net>
2022-06-06 23:03:43 +0100

    Putting app.vue back in

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -81,15 +81,6 @@ const toggleDarkMode = () => {
     localStorage.theme = "dark";
   }
 };
-
-// Whenever the user explicitly chooses light mode
-localStorage.theme = "light";
-
-// Whenever the user explicitly chooses dark mode
-localStorage.theme = "dark";
-
-// Whenever the user explicitly chooses to respect the OS preference
-localStorage.removeItem("theme");
 </script>
 
 <style>
