33d14ccf9c68cf72b33d292871fb9dfda871e41b
Digby Knight <digby@digby.net>
2022-06-09 21:40:31 +0100

    T|rying to fig BgImage

--- pages/search.vue
+++ pages/search.vue
@@ -75,7 +75,7 @@
                         :to="`/book/${item.isbn}/${item.title}?queryID=${item.__queryID}`"
                         class="h-full"
                       >
-                        <BgImage
+                        <BgImage2
                           :src="item.thumbnail"
                           :alt="item.title"
                           @click="sendEvent('conversion', item, 'Viewed')"
