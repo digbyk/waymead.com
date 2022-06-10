c7644c378430a7b0a4f7e3b740f6f5a7caf1753a
Digby Knight <digby@digby.net>
2022-06-10 13:34:12 +0100

    Slightly chaning header transparency

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     class="flex flex-col min-h-full w-full font-sans bg-light-100 dark:bg-gradient-to-br dark:from-slate-800 dark:to-slate-800 text-stone-700 dark:text-stone-300"
   >
     <header
-      class="container m-auto p-4 md:sticky md:top-0 z-10 dark:bg-slate-800/50 backdrop-filter backdrop-blur-lg border-b border-gray-700"
+      class="container m-auto p-4 md:sticky md:top-0 z-10 dark:bg-slate-800/60 backdrop-filter backdrop-blur-lg border-b border-gray-700"
     >
       <Header />
     </header>
