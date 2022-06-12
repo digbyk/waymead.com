a0ae1237b5c351ebc606ff88f1517f7f365ce5e4
Digby Knight <digby@digby.net>
2022-06-12 14:18:06 +0100

    Tidying up content

--- content/about.md
+++
@@ -1,3 +0,0 @@
-# Waymead - about us
-
-Hello, this is the about us page
---
+++ pages/[slug].vue
@@ -0,0 +1,22 @@
+<template>
+  <article
+    class="w-full prose dark:prose-invert max-w-none h-full flex place-items-center place-content-center"
+  >
+    <ContentDoc>
+      <template #not-found>
+        <div
+          class="flex flex-col place-content-center place-items-center h-full"
+        >
+          <div class="text-4rem md:text-12rem">404</div>
+          <p class="text-center">
+            Not sure how you got here, but you seem a little lost. Why don't you
+            go back to the <NuxtLink to="/">home page</NuxtLink> and start
+            again.
+          </p>
+        </div>
+      </template>
+    </ContentDoc>
+  </article>
+</template>
+
+<script setup></script>
