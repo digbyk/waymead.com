6a4a0c1100f48abab7796233ab9fc62358d8ce14
Digby Knight <digby@digby.net>
2022-06-15 13:57:44 +0100

    Fixing favicons

--- app.vue
+++ app.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script setup>
+import { useFavicon } from "@vueuse/core";
+const icon = useFavicon("favicon-196.png");
 useHead({
   titleTemplate: "%s | Waymead",
   htmlAttrs: {
@@ -13,6 +15,7 @@ useHead({
   viewport: "width=device-width, initial-scale=1",
   charset: "utf-8",
   meta: [{ name: "description", content: "waymead.com" }],
+  link: [{ rel: "icon", type: "image/ico", href: "/favicon-196.png" }],
 });
 </script>
 
