de12973a42735c5f94589d1e68c566cf04d8ec34
Digby Knight <digby@digby.net>
2022-06-19 06:46:59 +0100

    Adding launcher to debug - only debugger seems to work

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,8 +1,15 @@
+import { serverSupabaseClient } from "#supabase/server";
+import { serverSupabaseUser } from "#supabase/server";
+
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default defineEventHandler(async (event) => {
-  await sleep(2000);
+  const client = serverSupabaseClient(event);
+  const user = await serverSupabaseUser(event);
+  await sleep(1000);
   return {
     api: "it works",
+    user: user.email,
+    test: "test",
   };
 });
